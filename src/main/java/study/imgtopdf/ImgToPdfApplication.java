f1ce1e4df18254bf175dbfc5b9f3ae713802cba8
Geunsik Kim <geunsikevin@naver.com>
2024-10-08 17:34:20 +0900

    초기환경 구축

---
+++ src/main/java/study/imgtopdf/ImgToPdfApplication.java
@@ -0,0 +1,13 @@
+package study.imgtopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgToPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgToPdfApplication.class, args);
+    }
+
+}
